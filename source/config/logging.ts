c17f4a02ae954600b9c3a7c4b4e02231c5ba17b8
= <rknb.dev@gamail.com>
2022-10-28 11:45:16 +0530

    without colors js console beautificaton

--- source/config/logging.ts
+++ source/config/logging.ts
@@ -1,35 +1,32 @@
-import color from 'colors';
-var colors = require('colors');
-
 const info = (namespace: string, message: string, object?: any) => {
     if (object) {
-        console.info(`[${getTimeStamp()}]`, '[' + 'INFO'.green + ']', `[` + `${namespace}`.cyan + `] ${message}`, object);
+        console.info(`[${getTimeStamp()}] [INFO] [${namespace}] ${message}`, object);
     } else {
-        console.info(`[${getTimeStamp()}]`, '[' + 'INFO'.green + ']', `[` + `${namespace}`.cyan + `] ${message}`);
+        console.info(`[${getTimeStamp()}] [INFO] [${namespace}] ${message}`);
     }
 };
 
 const warn = (namespace: string, message: string, object?: any) => {
     if (object) {
-        console.warn(`[${getTimeStamp()}]`, '[' + 'WARN'.yellow + ']', `[` + `${namespace}`.cyan + `] ${message}`, object);
+        console.warn(`[${getTimeStamp()}] [WARN] [${namespace}] ${message}`, object);
     } else {
-        console.warn(`[${getTimeStamp()}]`, '[' + 'WARN'.yellow + ']', `[` + `${namespace}`.cyan + `] ${message}`);
+        console.warn(`[${getTimeStamp()}] [WARN] [${namespace}] ${message}`);
     }
 };
 
 const error = (namespace: string, message: string, object?: any) => {
     if (object) {
-        console.error(`[${getTimeStamp()}]`, '[' + 'ERROR'.red + ']', `[` + `${namespace}`.cyan + `] ${message}`, object);
+        console.error(`[${getTimeStamp()}] [ERROR] [${namespace}] ${message}`, object);
     } else {
-        console.error(`[${getTimeStamp()}]`, '[' + 'ERROR'.red + ']', `[` + `${namespace}`.cyan + `] ${message}`);
+        console.error(`[${getTimeStamp()}] [ERROR] [${namespace}] ${message}`);
     }
 };
 
 const debug = (namespace: string, message: string, object?: any) => {
     if (object) {
-        console.debug(`[${getTimeStamp()}]`, '[' + 'DEBUG'.blue + ']', `[` + `${namespace}`.cyan + `] ${message}`, object);
+        console.debug(`[${getTimeStamp()}] [DEBUG] [${namespace}] ${message}`, object);
     } else {
-        console.debug(`[${getTimeStamp()}]`, '[' + 'DEBUG'.blue + ']', `[` + `${namespace}`.cyan + `] ${message}`);
+        console.debug(`[${getTimeStamp()}] [DEBUG] [${namespace}] ${message}`);
     }
 };
 
